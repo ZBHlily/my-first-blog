0c79955c25c6828c67a107b8085c6ee6f5966abd
Lily <liang1220lily@gmail.com>
2021-11-26 12:17:22 -0500

    created an API

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import include, path
+from rest_framework import routers
+from . import views
+#from . import PostViewSet
+from blog.views import PostViewSet
+
+router = routers.DefaultRouter()
+router.register(r'post', PostViewSet)
+
+urlpatterns = [
+   path('', include(router.urls)),
+   path('', views.post_list, name='post_list')
+]
