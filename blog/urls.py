bf84a9423fb6c903de2d859253f4056ef8063ccd
Lily <liang1220lily@gmail.com>
2021-12-21 19:27:04 -0500

    created post list html

--- blog/urls.py
+++ blog/urls.py
@@ -4,12 +4,14 @@ from . import views
 #from . import PostViewSet
 from blog.views import PostViewSet, BookViewSet
 
+
 router = routers.DefaultRouter()
 router.register(r'post', PostViewSet)
 router.register(r'book', BookViewSet)
 
 urlpatterns = [
    path('', include(router.urls)),
-   path('', views.post_list, name='post_list'),
-   path('', views.book_list, name='book_list')
+   #to run it we do post_list
+   path('post_list', views.post_list, name='post_list')
+   #path('post_list', views.post_list, name='book_list')
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,12 +4,17 @@ from rest_framework import viewsets
 from blog.serializers import PostSerializer, BookSerializer 
 from blog.models import Post, Book
 
+from .models import Post
+from django.utils import timezone
+
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    #return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
@@ -17,3 +22,5 @@ class BookViewSet(viewsets.ModelViewSet):
 
 def book_list(request):
     return render(request, 'blog/book_list.html', {})
+
+Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
