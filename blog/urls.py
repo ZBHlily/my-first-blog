0610953b2a4ea39398e068bf6eaf1f717eb703e5
Lily <liang1220lily@gmail.com>
2021-12-24 16:49:10 -0500

    semi-created django forms

--- blog/urls.py
+++ blog/urls.py
@@ -14,6 +14,8 @@ urlpatterns = [
    #to run it we do post_list
    path('post_list', views.post_list, name='post_list'),
    #path('post_list', views.post_list, name='book_list')
-   path('post_detail/<int:pk>/', views.post_detail, name='post_detail')
+   path('post_detail/<int:pk>/', views.post_detail, name='post_detail'),
+   path('post/new/', views.post_new, name='post_new'),
+   path('post/<int:pk>/edit/', views.post_edit, name='post_edit')
 ]
 
